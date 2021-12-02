9ec2db3f6c051a5e5899561f28b947c6ba9dd786
Yonka 2019 <yonka2003@gmail.com>
2021-12-02 18:48:56 +0200

    a

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace LinkedListHW
             // QUESTION 2
             Node<int> list = CreateLinkedList(1, 9, 2, 8, 5, 5);
             Console.WriteLine(list);
+
             if (List10(list))
             {
                 list.InsertEnd(6);
